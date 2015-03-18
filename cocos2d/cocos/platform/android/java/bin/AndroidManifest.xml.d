44b8661585a0309a6fec868caea1ee8a147193d2
doyoubi <doyoubihgx@gmail.com>
2015-03-18 13:32:27 +0800

    add platform dir

---
+++ cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\workplace\minigame\joker\cocos2d\cocos\platform\android\java\bin\AndroidManifest.xml : \
+D:\workplace\minigame\joker\cocos2d\cocos\platform\android\java\AndroidManifest.xml \
---
+++ cocos2d/cocos/platform/android/java/gen/R.java.d
@@ -0,0 +1,1 @@
+ : D:\workplace\minigame\joker\cocos2d\cocos\platform\android\java\bin\AndroidManifest.xml \
